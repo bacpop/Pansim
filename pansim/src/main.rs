a734585109332cfbfda61e5fc13c06815da6cf55
Samuel Horsfield <shorsfield@ebi.ac.uk>
2024-03-26 18:14:52 +0000

    Corrects build time errors

--- pansim/src/main.rs
+++ pansim/src/main.rs
@@ -1,18 +1,33 @@
 extern crate rand;
+extern crate statrs;
+
+use statrs::distribution::{Poisson, Discrete};
+use statrs::statistics::Distribution;
+use statrs::prec;
 
 use rand::distributions::WeightedIndex;
+use rand::prelude::*;
 use rand::Rng;
+use rand::{seq::IteratorRandom, thread_rng};
+
+fn sample_with_weighted_replacement(weights: &Vec<f64>) -> usize {
+    let dist = WeightedIndex::new(weights).unwrap();
+    let mut rng = thread_rng();
+    dist.sample(&mut rng)
+}
+
+fn normalize_vector(vec: &mut Vec<f64>) {
+    let sum: f64 = vec.iter().sum();
+
+    for val in vec.iter_mut() {
+        *val /= sum;
+    }
+}
 
 struct Population {
     size: usize,
     allele_count: usize,
     pop: Vec<Vec<i32>>,
-}
-
-fn sample_with_weighted_replacement(weights: &[f64]) -> usize {
-    let dist = WeightedIndex::new(weights).unwrap();
-    let mut rng = rand::thread_rng();
-    dist.sample(&mut rng)
 }
 
 impl Population {
@@ -32,38 +47,38 @@ impl Population {
         }
     }
 
-    fn next_generation(&mut self, &Vec<usize> sample) {
+    fn next_generation(&mut self, sample : &Vec<usize>) {
         
         let mut next_pop: Vec<Vec<i32>> = sample.iter().map(|&i| self.pop[i].clone()).collect();
 
         self.pop = next_pop;
     }
 
-    fn mutate_alleles(&self, weights : Vec<f64>) {
+    fn mutate_alleles(&self, weights : Vec<f64>, mutations : i32, max_variants : i32) {
+        let mut rng = rand::thread_rng();
+        
         // iterate over each genome
         for i in 0..self.size {
+            // sample from Poisson distribution for number of sites to mutate in this isolate
+            let poisson = Poisson::new(mutations as f64).unwrap();
+            let n_sites = rng.sample(poisson) as usize;
             // iterate for number of mutations required to reach mutation rate
+            for j in 0..n_sites {
+                // sample new site to mutate
+                let mutant_site : usize = sample_with_weighted_replacement(&weights);
+                
+                // get possible values to mutate to, must be different from current value
+                let mut values = Vec::from_iter(0..(max_variants - 1));
+                let value = self.pop[i][j];
+                values.retain(|&x| x != value);
 
-        }
+                // sample new allele
+                let mut new_allele = values.iter().choose_multiple(&mut rng, 1)[0];
 
-        let sampled_indices: Vec<usize> = (0..num_samples)
-        .map(|_| sample_with_weighted_replacement(&weights))
-        .collect();
-    }
-
-    fn sample_allele(&self) -> usize {
-        let mut rng = rand::thread_rng();
-        let mut cumulative_freq = 0.0;
-        let r = rng.gen::<f64>();
-
-        for i in 0..self.allele_count {
-            cumulative_freq += self.allele_freq[i];
-            if r <= cumulative_freq {
-                return i;
+                // set value in place
+                self.pop[i][j] = *new_allele;
             }
         }
-
-        self.allele_count - 1 // Should not reach here
     }
 }
 
@@ -72,29 +87,34 @@ fn main() {
     let core_size = 100;
     let pan_size = 100;
     let n_gen = 10;
+
+    // core and pangenome mutation rates
+    let core_mu = 0.01;
+    let pan_mu = 0.001;
+
+    // calculate number of mutations per genome per generation
+    let n_core_mutations = ((core_size as f64 * core_mu) / n_gen as f64) / 2.0 ;
+    let n_pan_mutations = ((pan_size as f64 * pan_mu) / n_gen as f64) / 2.0;
+
+    let mut core_weights : Vec<f64> = vec![1.0; core_size];
+    normalize_vector(&mut core_weights);
+
+    let mut pan_weights : Vec<f64> = vec![1.0; core_size];
+    normalize_vector(&mut pan_weights);
     
     let mut core_genome = Population::new(pop_size, core_size, 4); // core genome alignment
-    let mut pan_genome = Population::new(pop_size, pan_size), 2; // pangenome alignment
-    
-
-    println!("Initial allele frequencies: {:?}", pop.allele_freq);
+    let mut pan_genome = Population::new(pop_size, pan_size, 2); // pangenome alignment
 
     let mut rng = rand::thread_rng();
 
-    for _ in 0..n_gen { // Run for 10 generations
+    for _ in 0..n_gen { // Run for n_gen generations
+        // mutate core genome
+        core_genome.mutate_alleles(core_weights, n_core_mutations as i32, 4);
+        pan_genome.mutate_alleles(pan_weights, n_pan_mutations as i32, 2);
 
-        
+        // sample new individuals
         let sampled_individuals: Vec<usize> = (0..pop_size).map(|_| rng.gen_range(0..pop_size)).collect();
-        
-
-
-
-        pop.next_generation();
-        println!("Next generation allele frequencies: {:?}", pop.allele_freq);
-    }
-
-    // Sample some alleles from the population
-    for _ in 0..10 {
-        println!("Sampled allele: {}", pop.sample_allele());
+        core_genome.next_generation(& sampled_individuals);
+        pan_genome.next_generation(& sampled_individuals);
     }
 }
